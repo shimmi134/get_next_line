3ee38a4559bbcdf1b88fb94c7a414063a35599c1
Sami Himi Ben Alilou <shimi-be@car11s6.42barcelona.com>
2024-11-23 22:40:32 +0100

    LAst commit

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: shimi-be <shimi-be@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 18:34:32 by shimi-be          #+#    #+#             */
-/*   Updated: 2024/11/23 17:35:25 by shimi-be         ###   ########.fr       */
+/*   Updated: 2024/11/23 17:48:36 by shimi-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char *get_next_line(int fd)
 	char	*arr;
 	char	*temp;
 
-	if (fd < 0 || BUFFER_SIZE < 0)//|| read(fd,buff,BUFFER_SIZE) < 0
+	if (fd < 0 || BUFFER_SIZE < 0)
 		return (NULL);
 	temp = NULL;
 	if (!stash || !ft_strchr(stash,0))
